46bb73bb9807f832abded29b05b2e2475692f4cd
Dmitri Soshnikov <dmitri@soshnikov.com>
2016-12-01 10:18:40 +0000

    Experiment 2

--- PostDeployScripts/copyDll.cmd
+++ PostDeployScripts/copyDll.cmd
@@ -1,4 +1,4 @@
-echo Doing DLL copy... >> ..\deployment.log
-dir ..\wwwroot\MediatorLib
-dir MediatorLib
-copy ..\wwwroot\MediatorLib\bin\Release\MediatorLib.dll ..\wwwroot\messages\bin\MediatorLib.dll
+rem echo Doing DLL copy... >> ..\deployment.log
+rem dir ..\wwwroot\MediatorLib
+rem dir MediatorLib
+rem copy ..\wwwroot\MediatorLib\bin\Release\MediatorLib.dll ..\wwwroot\messages\bin\MediatorLib.dll
