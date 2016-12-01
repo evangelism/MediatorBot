665dab623d7484d972579d1cf74ce868b57e8963
Dmitri Soshnikov <dmitri@soshnikov.com>
2016-12-01 11:11:45 +0000

    Change to new bot model

---
+++ MediatorBot/MediatorBot/MediatorDialog.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MediatorBot
+{
+    [Serializable]
+    public class MediatorDialog : IDialog<string>
+    {
+
+        public float SentimentScore = 0;
+        public List<string> transcript = new List<string>();
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = await argument;
+            if (message.Text=="!list")
+            {
+                var b = new StringBuilder();
+                transcript.ForEach(x => b.AppendLine(x));
+                await context.PostAsync(b.ToString());
+                transcript.Clear();
+            }
+        }
+    }
+}
